ccdff6144936cbe8bdf074e48e4c3e35b89b2da2
poojadalai <poojadalai@gmail.com>
2022-02-15 14:53:18 +0100

    add hide functionality on click of navbaritem

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const navIcon = document.querySelector('.icons');
-
+const items = document.querySelectorAll('.item a');
+var mobileLinks = document.querySelector(".mobile-links");
+   
 
 navIcon.addEventListener("click", function(){
-    var mobileLinks = document.querySelector(".mobile-links");
-    if (mobileLinks.style.display == "block") {
+     if (mobileLinks.style.display == "block") {
         mobileLinks.style.display = "none";
     } else {
         mobileLinks.style.display = "block";
@@ -12,4 +13,14 @@ navIcon.addEventListener("click", function(){
 
 function myFunction(x) {
     x.classList.toggle("fa-thumbs-down");
-  }
+}
+
+for(let  i =0 ; i<items.length; i++){
+    let item = items[i];
+    item.addEventListener('click', function(){
+        mobileLinks.style.display = "none";
+    })
+}
+
+
+
